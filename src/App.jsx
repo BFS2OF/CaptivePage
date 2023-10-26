747628ed420071f6dbf8800bd04d769b0855d9aa
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-26 21:34:47 +0200

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
-export const App = () => {
+import "@/styles/fonts.sass";
+import "@/styles/main.sass";
+import Header from "@/components/Header";
+import MainContent from "@/components/MainContent";
+import Questions from "@/questions.json";
+import {useEffect, useState} from "react";
 
+export const App = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(null);
+
+    const rePickQuestion = () => {
+        const randomIndex = Math.floor(Math.random() * Questions.length);
+
+        if (currentQuestion === randomIndex) return rePickQuestion();
+
+        setCurrentQuestion(randomIndex);
+    }
+
+    useEffect(() => {
+        rePickQuestion();
+    }, []);
+
+    if (currentQuestion === null) return null;
+
+    return (
+        <>
+            <Header />
+
+            <MainContent question={Questions[currentQuestion]} rePickQuestion={rePickQuestion}/>
+        </>
+    );
 }
