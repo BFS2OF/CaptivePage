7479fafd393d250a66f301a80fdec3c7c355cb79
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-26 21:32:27 +0200

    Created the Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./styles.sass";
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="/assets/images/bs2ab.png" alt="BS2AB" />
+            <img src="/assets/images/logo.png" alt="BFS2OF" />
+        </header>
+    )
+}
