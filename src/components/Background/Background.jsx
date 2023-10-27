ac17f0ae298e7be3ba85be65065035b101b4ff77
Mathias Wagner <germannewsmaker@gmail.com>
2023-10-27 06:50:44 +0200

    Created the Background.jsx

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import "./styles.sass";
+
+export const Background = () => {
+    return (
+        <div className="background">
+            <div className="shape" />
+            <div className="shape" />
+        </div>
+    )
+}
